7f6b9acbae4aa2b3a20a72732da89125b86e1fc5
lucwssouza <lucassnoze1@gmail.com>
2024-02-19 11:21:18 -0300

    up

---
+++ src/subtracao/subtrair.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package subtracao;
+
+/**
+ *
+ * @author 924535
+ */
+public class subtrair {
+    public static int conta(int a, int b){
+        int x = a - b;
+        return x;
+    }
+}
